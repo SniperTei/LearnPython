8fdff2bbf6f55e22ff55c6741166f02c60ca21b8
Sniper <sniper_zheng@163.com>
2023-03-18 16:05:16 +0800

    qrcode and learn drf

--- QRCode/QRCodeManager.py
+++ QRCode/QRCodeManager.py
@@ -1,12 +1,13 @@
 import qrcode
 import os
-
+import sys
+from PIL import Image
 
 class QRCodeManager:
     def __init__(self, data):
         self.data = data
 
-    def generate_qr_code(self, fill_color="black", back_color="white", imgname="qrcode.png"):
+    def generate_qr_code(self, fill_color="black", back_color="white", img_name="qrcode.png"):
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -15,11 +16,13 @@ class QRCodeManager:
         )
         qr.add_data(self.data)
         qr.make(fit=True)
+        # 保存二维码图片名 打开和删除时候要用到
+        self.img_name = img_name
 
         img = qr.make_image(fill_color=fill_color, back_color=back_color)
-        img.save(imgname)
+        img.save(img_name)
     
-    def generate_qr_code_with_logo(self, logo_path, fill_color="black", back_color="white", imgname="qrcode.png"):
+    def generate_qr_code_with_logo(self, logo_path, fill_color="black", back_color="white", img_name="qrcode.png"):
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -30,13 +33,18 @@ class QRCodeManager:
         qr.make(fit=True)
         
         img = qr.make_image(fill_color=fill_color, back_color=back_color)
-        img.save(imgname)
+        img.save(img_name)
 
     def open_qr_code(self):
+        # 用系统默认的图片查看器打开图片
         print("Opening QR Code...")
+        spect= Image.open(self.img_name)
+        spect.show()
 
     def delete_qr_code(self):
-        os.remove("qrcode.png")
+        # 删除图片
+        print("Deleting QR Code...")
+        os.remove(self.img_name)
         
     def __repr__(self):
         return "QRCodeManager({})".format(self.data)
@@ -44,4 +52,16 @@ class QRCodeManager:
     def __str__(self):
         return "QRCodeManager({})".format(self.data)
 
-qr = QRCodeManager({"name": "John", "age": 30})
+
+def main():
+    print("main ")
+    qr_data = sys.argv[1]
+    if qr_data:
+        qr = QRCodeManager(qr_data)
+        qr.generate_qr_code()
+        qr.open_qr_code()
+        print("QR Code Generated!")
+        print(f"{qr}")
+
+if __name__ == "__main__":
+    main()
