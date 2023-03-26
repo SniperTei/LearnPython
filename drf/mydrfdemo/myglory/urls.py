c93d88b49baa4e8067a94c934e64a13ef5a4ca09
Sniper <sniper_zheng@163.com>
2023-03-26 10:54:46 +0800

    detail and modify

--- drf/mydrfdemo/myglory/urls.py
+++ drf/mydrfdemo/myglory/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from myglory.views import GloryHeroList
+from myglory.views import GloryHeroList, GloryHeroDetail
 # from rest_framework import routers
 
 # routers = routers.DefaultRouter()
 
 urlpatterns = [
     path('gloryherolist/', GloryHeroList.as_view()),
-    path('gloryherolist/<int:_id>', GloryHeroList.as_view()),
+    path('gloryherolist/<int:id>', GloryHeroDetail.as_view()),
 ]
--- drf/mydrfdemo/myglory/views.py
+++ drf/mydrfdemo/myglory/views.py
@@ -21,32 +21,43 @@ class GloryHeroList(APIView):
         return Response(serializer.errors, status=200)
     
     def put(self, request):
-        # serializer = GloryHeroSerializer(data=request.data)
-        # try:
-        #     serializer.is_valid(raise_exception=True)
-        #     print(serializer.validated_data)
-        #     gloryheros = GloryHero.objects.get(id=id).update(**serializer.validated_data)
-        #     print("gloryheros: ", gloryheros)
-            # serializer = GloryHeroSerializer(gloryheros)
+        print("request.data :", request.data)
+        serializer = GloryHeroSerializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+            # print(serializer.validated_data)
+            # 获取到要修改的数据
+            gloryHero = GloryHero.objects.get(id=serializer.validated_data['id'])
+            print("gloryHero: ", gloryHero)
             return Response({"aaa":"123"}, status=200)
-        # except Exception as e:
-        #     return Response(serializer.errors, status=200)
+        except Exception as e:
+            print("e: ", e.args)
+            return Response({"error" : "error le"}, status=200)
     
-    # def put(self, request, id):
-    #     print("put id: ", id)
-    #     serializer = GloryHeroSerializer(data=request.data)
-    #     try:
-    #         serializer.is_valid(raise_exception=True)
-    #         print(serializer.validated_data)
-    #         gloryheros = GloryHero.objects.get(id=id).update(**serializer.validated_data)
-    #         print("gloryheros: ", gloryheros)
-    #         # serializer = GloryHeroSerializer(gloryheros)
-    #         return Response(serializer.data, status=200)
-    #     except Exception as e:
-    #         return Response(serializer.errors, status=200)
+    
+class GloryHeroDetail(APIView):
+    
+    def get(self, request, id):
+        gloryheros = GloryHero.objects.get(id=id)
+        serializer = GloryHeroSerializer(gloryheros)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        print("put id: ", id)
+        serializer = GloryHeroSerializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+            # n = GloryHero.objects.get(id=id).update(**serializer.validated_data)
+            n = GloryHero.objects.filter(id=id).update(**serializer.validated_data)
+            print("n: ", n)
+            gloryhero = GloryHero.objects.get(id=id)
+            # 保存
+            serializer = GloryHeroSerializer(gloryhero)
+            return Response(serializer.data, status=200)
+        except Exception as e:
+            return Response(serializer.errors, status=200)
     
     def delete(self, request, id):
         gloryheros = GloryHero.objects.get(id=id)
         gloryheros.delete()
         return Response(status=200)
-    
