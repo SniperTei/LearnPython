98445637fb25376f8849737c5ee0ce53999408b4
Sniper <sniper_zheng@163.com>
2023-03-28 00:00:01 +0800

    校验

--- drf/mydrfdemo/myglory/serializers.py
+++ drf/mydrfdemo/myglory/serializers.py
@@ -4,6 +4,12 @@ from myglory.models import GloryHero
 
 class GloryHeroSerializer(serializers.ModelSerializer):
     
+    # 校验熟练度字段
+    def validate_proficiency(self, value):
+        if value < 0 or value > 10000:
+            raise serializers.ValidationError("熟练度必须在0~10000之间")
+        return value
+    
     class Meta:
         model = GloryHero
         fields = "__all__"
