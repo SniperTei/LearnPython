aa6e43ad3dcd20a94123309d7f89b55a07d48e1c
Sniper <sniper_zheng@163.com>
2024-05-20 00:12:02 +0800

    1

---
+++ drf/mydrfdemo/snipermovie/accounts/models.py
@@ -0,0 +1,13 @@
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
+
+# class CustomUser(AbstractUser):
+#   # email = models.EmailField(unique=True)
+#   # Add your custom fields here
+#   bio = models.TextField(blank=True)
+#   location = models.CharField(max_length=255, blank=True)
+#   # Add more fields as needed
+#   account_type = models.CharField(max_length=255, blank=True)
+
+#   def __str__(self):
+#     return self.username
