1d41ecbe41063954704c0f6f4484565f137fda48
Sniper <sniper_zheng@163.com>
2024-05-28 09:24:01 +0800

    book

--- drf/mydrfdemo/book/models.py
+++ drf/mydrfdemo/book/models.py
@@ -7,6 +7,8 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     publisher = models.CharField(max_length=100)
     pubdate = models.DateField()
+    # 类型(0: 未知, 1: 科幻, 2: 悬疑, 3: 爱情, 4: 惊悚, 5: 恐怖, 6: 励志, 7: 历史, 8: 传记, 9: 其他)
+    type = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.CharField(max_length=100)
